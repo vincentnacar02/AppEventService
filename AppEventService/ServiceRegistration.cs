7266b67c0558ea6851fb9e1fee1d9cc225a9d586
Vincent Nacar <vincent.nacar@contractor.fitlg.com>
2023-09-16 12:59:23 +0800

    added service registration

---
+++ AppEventService/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEventService
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddAppEventService<T>(this IServiceCollection services) where T : IAppEvent
+        {
+            services.AddSingleton<IAppEventService<T>, AppEventService<T>>();
+            return services;
+        }
+    }
+}
